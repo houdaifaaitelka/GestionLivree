5efd433e160b947b7d519572ff39d13f484b1da6
Houdaifa ait el kadi <houdaifa.aitelkadi@monboreal.ca>
2024-10-19 10:16:44 -0700

    Ajout de la classe Livre

---
+++ Livre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Livre
+{
+    public string Titre { get; set; }
+    public string Auteur { get; set; }
+    public int NombrePages { get; set; }
+
+    public Livre(string titre, string auteur, int nbrPages)
+    {
+        Titre = titre;
+        Auteur = auteur;
+        NombrePages = nbrPages;
+    }
+
+    public void AfficherDetails()
+    {
+        Console.WriteLine($"Titre: {Titre}, Auteur: {Auteur}, Nombre de Pages: {NombrePages}");
+    }
+}
