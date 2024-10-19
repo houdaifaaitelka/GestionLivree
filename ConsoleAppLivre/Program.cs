eae6b5758f8f92fbaa392ccd17193ec6756debc2
Houdaifa ait el kadi <houdaifa.aitelkadi@monboreal.ca>
2024-10-19 11:03:07 -0700

    Message décrivant les modifications

--- ConsoleAppLivre/Program.cs
+++ ConsoleAppLivre/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿
+using MonProjet;
+internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Revue revue1 = new Revue("Titre Revue 1", "Auteur 1", 100, 1);
+        Revue revue2 = new Revue("Titre Revue 2", "Auteur 2", 150, 2);
+        Revue revue3 = new Revue("Titre Revue 3", "Auteur 3", 120, 3);
+
+        revue1.AfficherDetails();
+        revue2.AfficherDetails();
+        revue3.AfficherDetails();
+
     }
 }
---
+++ Revue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace MonProjet
+{
+    public class Revue : Livre
+    {
+        public int Numero { get; set; }
+        public int Annee { get; set; }
+        public Revue(string titre, string auteur, int nombreDePages, int numero, int annee)
+            : base(titre, auteur, nombreDePages)
+        {
+            Numero = numero;
+            Annee = annee;
+        }
+        public override void AfficherDetails()
+        {
+            base.AfficherDetails();
+            Console.WriteLine($"Numéro de la revue: {Numero}");
+            Console.WriteLine($"Année de publication: {Annee}");
+        }
+
+    }
+}
